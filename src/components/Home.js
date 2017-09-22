5badc487f7ba7e6ca70bcbf2d8438613c94829d3
ZhaoTong <zhaotong@meican.com>
2017-09-23 01:51:01 +0800

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ import styles from './styles.scss'
 class Home extends Component {
 	render() {
 		return (
-			<div className={styles.home}>T.Zhao FE Starter</div>
+			<div>
+				<div className={styles.home}>T.Zhao FE Starter</div>
+				<div className={styles.input} data-content="123"></div>
+			</div>
 		)
 	}
 }
